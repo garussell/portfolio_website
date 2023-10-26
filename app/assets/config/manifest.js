4fbd448ecca69bdf4fb85f414a0c9244356ea957
Allen Russell <garyrusselljr@gmail.com>
2023-10-26 14:46:31 -0600

    initial commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,19 @@
+// = link_tree ../images
+// = link_directory ../stylesheets .css
+// = link_tree ../../javascript .js
+// = link_tree ../../../vendor/javascript .js
+
+//= link application.js
+//= link styles.css
+//= link github-calendar/dist/github-calendar-responsive.css
+//= link github-activity/src/github-activity.css
+
+//= link js/all.js
+
+//= link popper.min.js
+//= link bootstrap/js/bootstrap.min.js
+//= link vanilla-rss/dist/rss.global.min.js
+//= link dark-mode-switch/dark-mode-switch.min.js
+//= link github-calendar/dist/github-calendar.min.js
+//= link github-activity/src/github-activity.js
+//= link main.js
