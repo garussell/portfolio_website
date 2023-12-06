17c581551b76e78f08c9d57ef900d218b5d3d07f
Allen Russell <garyrusselljr@gmail.com>
2023-12-06 16:37:01 -0700

    removed github stylesheet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.precompile += %w( js/all.js )
-  config.assets.precompile += %w( github-calendar/dist/github-calendar-responsive.css )
+
   config.assets.precompile += %w( bootstrap/css/bootstrap.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
