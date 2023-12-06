d8741155b265c4fc88eb726af84ddf6c1ad10692
Allen Russell <garyrusselljr@gmail.com>
2023-12-06 16:31:32 -0700

    sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,14 +25,14 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.precompile += %w( js/all.js )
   config.assets.precompile += %w( github-calendar/dist/github-calendar-responsive.css )
   config.assets.precompile += %w( bootstrap/css/bootstrap.css )
-  
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
