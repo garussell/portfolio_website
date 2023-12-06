19f3d08fdeebdcc3631dfe785827d8bab6970d27
Allen Russell <garyrusselljr@gmail.com>
2023-12-06 16:23:20 -0700

    production config

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,6 +6,85 @@
 //= link application.js
 //= link styles.css
 
+//= link js/all.js
+//= link popper.min.js
+
+//= link bootstrap/css/bootstrap-grid.css
+//= link bootstrap/css/bootstrap-grid.css.map
+//= link bootstrap/css/bootstrap-grid.min.css
+//= link bootstrap/css/bootstrap-grid.min.css.map
+//= link bootstrap/css/bootstrap-grid.rtl.css
+//= link bootstrap/css/bootstrap-grid.rtl.css.map
+//= link bootstrap/css/bootstrap-grid.rtl.min.css
+//= link bootstrap/css/bootstrap-grid.rtl.min.css.map
+//= link bootstrap/css/bootstrap-reboot.css
+//= link bootstrap/css/bootstrap-reboot.css.map
+//= link bootstrap/css/bootstrap-reboot.min.css
+//= link bootstrap/css/bootstrap-reboot.min.css.map
+//= link bootstrap/css/bootstrap-reboot.rtl.css
+//= link bootstrap/css/bootstrap-reboot.rtl.css.map
+//= link bootstrap/css/bootstrap-reboot.rtl.min.css
+//= link bootstrap/css/bootstrap-reboot.rtl.min.css.map
+//= link bootstrap/css/bootstrap-utilities.css
+//= link bootstrap/css/bootstrap-utilities.css.map
+//= link bootstrap/css/bootstrap-utilities.min.css
+//= link bootstrap/css/bootstrap-utilities.min.css.map
+//= link bootstrap/css/bootstrap-utilities.rtl.css
+//= link bootstrap/css/bootstrap-utilities.rtl.css.map
+//= link bootstrap/css/bootstrap-utilities.rtl.min.css
+//= link bootstrap/css/bootstrap-utilities.rtl.min.css.map
+//= link bootstrap/css/bootstrap.css
+//= link bootstrap/css/bootstrap.css.map
+//= link bootstrap/css/bootstrap.min.css
+//= link bootstrap/css/bootstrap.min.css.map
+//= link bootstrap/css/bootstrap.rtl.css
+//= link bootstrap/css/bootstrap.rtl.css.map
+//= link bootstrap/css/bootstrap.rtl.min.css
+//= link bootstrap/css/bootstrap.rtl.min.css.map
+
+
+//= link bootstrap/js/bootstrap.bundle.js
+//= link bootstrap/js/bootstrap.bundle.js.map
+//= link bootstrap/js/bootstrap.bundle.min.js
+//= link bootstrap/js/bootstrap.bundle.min.js.map
+//= link bootstrap/js/bootstrap.esm.js
+//= link bootstrap/js/bootstrap.esm.js.map
+//= link bootstrap/js/bootstrap.esm.min.js
+//= link bootstrap/js/bootstrap.esm.min.js.map
+//= link bootstrap/js/bootstrap.js
+//= link bootstrap/js/bootstrap.js.map
+//= link bootstrap/js/bootstrap.min.js
+//= link bootstrap/js/bootstrap.bundle.min.js
+
+
+//= link css/all.css
+//= link css/all.css.map
+//= link css/all.min.css
+//= link css/all.min.css.map
+//= link css/brands.css
+//= link css/brands.css.map
+//= link css/brands.min.css
+//= link css/brands.min.css.map
+//= link css/fontawesome.css
+//= link css/fontawesome.css.map
+//= link css/fontawesome.min.css
+//= link css/fontawesome.min.css.map
+//= link css/regular.css
+//= link css/regular.css.map
+//= link css/regular.min.css  
+//= link css/regular.min.css.map
+//= link css/solid.css
+//= link css/solid.css.map
+//= link css/solid.min.css
+//= link css/solid.min.css.map
+//= link css/svg-with-js.css
+//= link css/svg-with-js.css.map
+//= link css/svg-with-js.min.css
+//= link css/svg-with-js.min.css.map
+//= link css/v4-shims.css
+//= link css/v4-shims.css.map
+//= link css/v4-shims.min.css
+//= link css/v4-shims.min.css.map
 
 
 
