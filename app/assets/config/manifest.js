3b0595206c92331d5f7582e28d8f12f935f2c91c
Allen Russell <garyrusselljr@gmail.com>
2023-12-07 09:37:58 -0700

    remove bootstrap items

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -9,54 +9,6 @@
 //= link js/all.js
 //= link popper.min.js
 
-//= link bootstrap/css/bootstrap-grid.css
-//= link bootstrap/css/bootstrap-grid.css.map
-//= link bootstrap/css/bootstrap-grid.min.css
-//= link bootstrap/css/bootstrap-grid.min.css.map
-//= link bootstrap/css/bootstrap-grid.rtl.css
-//= link bootstrap/css/bootstrap-grid.rtl.css.map
-//= link bootstrap/css/bootstrap-grid.rtl.min.css
-//= link bootstrap/css/bootstrap-grid.rtl.min.css.map
-//= link bootstrap/css/bootstrap-reboot.css
-//= link bootstrap/css/bootstrap-reboot.css.map
-//= link bootstrap/css/bootstrap-reboot.min.css
-//= link bootstrap/css/bootstrap-reboot.min.css.map
-//= link bootstrap/css/bootstrap-reboot.rtl.css
-//= link bootstrap/css/bootstrap-reboot.rtl.css.map
-//= link bootstrap/css/bootstrap-reboot.rtl.min.css
-//= link bootstrap/css/bootstrap-reboot.rtl.min.css.map
-//= link bootstrap/css/bootstrap-utilities.css
-//= link bootstrap/css/bootstrap-utilities.css.map
-//= link bootstrap/css/bootstrap-utilities.min.css
-//= link bootstrap/css/bootstrap-utilities.min.css.map
-//= link bootstrap/css/bootstrap-utilities.rtl.css
-//= link bootstrap/css/bootstrap-utilities.rtl.css.map
-//= link bootstrap/css/bootstrap-utilities.rtl.min.css
-//= link bootstrap/css/bootstrap-utilities.rtl.min.css.map
-//= link bootstrap/css/bootstrap.css
-//= link bootstrap/css/bootstrap.css.map
-//= link bootstrap/css/bootstrap.min.css
-//= link bootstrap/css/bootstrap.min.css.map
-//= link bootstrap/css/bootstrap.rtl.css
-//= link bootstrap/css/bootstrap.rtl.css.map
-//= link bootstrap/css/bootstrap.rtl.min.css
-//= link bootstrap/css/bootstrap.rtl.min.css.map
-
-
-//= link bootstrap/js/bootstrap.bundle.js
-//= link bootstrap/js/bootstrap.bundle.js.map
-//= link bootstrap/js/bootstrap.bundle.min.js
-//= link bootstrap/js/bootstrap.bundle.min.js.map
-//= link bootstrap/js/bootstrap.esm.js
-//= link bootstrap/js/bootstrap.esm.js.map
-//= link bootstrap/js/bootstrap.esm.min.js
-//= link bootstrap/js/bootstrap.esm.min.js.map
-//= link bootstrap/js/bootstrap.js
-//= link bootstrap/js/bootstrap.js.map
-//= link bootstrap/js/bootstrap.min.js
-//= link bootstrap/js/bootstrap.bundle.min.js
-
-
 //= link css/all.css
 //= link css/all.css.map
 //= link css/all.min.css
